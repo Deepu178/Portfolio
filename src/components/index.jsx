bae554c525a496c6155e6ac59221b43de0f74f77
Deependra Kumar <deependrarajpoot178@gmail.com>
2023-07-01 02:10:02 +0530

    add resources

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "./Navbar/index";
+import { Hero } from "./Hero/index";
 import { Projectone } from "./Projects/Projectone";
 import { Footer } from "./Footer/index";
 import { Contact } from "./Contact/index";
 
 export {
     Navbar,
+    Hero,
     Projectone,
     Contact,
     Footer
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Navbar, Projectone, Footer } from '../../components/index';
+import { Navbar, Hero, Projectone, Footer } from '../../components/index';
 
 export const Home = () => {
   return (
     <div className='bg-black text-white'>
       <Navbar />
+      <Hero />
       <Projectone />
       <Footer />
     </div>
